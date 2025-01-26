a0ff7aac442b76095e4462e30bd21956b131fc23
Erik Thiede <ehthiede@gmail.com>
2025-01-26 18:10:49 -0500

    Updated test file

--- test_hw_1.py
+++ test_hw_1.py
@@ -23,51 +23,52 @@ def test_add_mixed_types():
 
 
 def test_k_equals_0():
-    assert (hw_1.first_k_fibonacci_numbers(0) == []).all()
+    assert np.allclose(hw_1.first_k_fibonacci_numbers(0), np.array([]))
 
 
 def test_k_equals_1():
-    assert hw_1.first_k_fibonacci_numbers(1) == [0]
+    assert np.allclose(hw_1.first_k_fibonacci_numbers(1), np.array([0]))
 
 
 def test_k_equals_2():
-    assert (hw_1.first_k_fibonacci_numbers(2) == [0, 1]).all()
+    assert np.allclose(hw_1.first_k_fibonacci_numbers(2), np.array([0, 1]))
 
 
 def test_k_equals_5():
-    assert (hw_1.first_k_fibonacci_numbers(5) == [0, 1, 1, 2, 3]).all()
+    assert np.allclose(hw_1.first_k_fibonacci_numbers(5) , np.array([0, 1, 1, 2, 3]))
 
 
 def test_k_equals_10():
-    assert (
-        hw_1.first_k_fibonacci_numbers(10) == [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
-    ).all()
+    np.allclose(
+        hw_1.first_k_fibonacci_numbers(10),
+        np.array([0, 1, 1, 2, 3, 5, 8, 13, 21, 34])
+    )
 
 
 def test_single_array():
-    assert hw_1.return_last_column(np.array([[1, 2, 3]])) == [3]
+    assert np.allclose(hw_1.return_last_column(np.array([[1, 2, 3]])), [3])
 
 
 def test_3x3_matrix():
     matrix = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
-    assert (hw_1.return_last_column(matrix) == [3, 6, 9]).all()
+    assert np.allclose(hw_1.return_last_column(matrix), [3, 6, 9])
 
 
 def test_4x2_matrix():
     matrix = np.array([[1, 2], [3, 4], [5, 6], [7, 8]])
-    assert (hw_1.return_last_column(matrix) == [2, 4, 6, 8]).all()
+    assert np.allclose(hw_1.return_last_column(matrix), [2, 4, 6, 8])
 
 
 def test_no_sixes():
-    assert hw_1.find_all_sixes([1, 2, 3, 4, 5]) == []
+    assert np.allclose(hw_1.find_all_sixes([1, 2, 3, 4, 5]), [])
 
 
 def test_three_sixes():
-    assert hw_1.find_all_sixes([1, 6, 2, 13, 6, 4, 6]) == [1, 4, 6]
+    assert np.allclose(hw_1.find_all_sixes([1, 6, 2, 13, 6, 4, 6]),  [1, 4, 6])
 
 
 def test_all_sixes():
-    assert hw_1.find_all_sixes([6, 6, 6, 6, 6]) == [0, 1, 2, 3, 4]
+    assert np.allclose(hw_1.find_all_sixes([6, 6, 6, 6, 6]),  [0, 1, 2, 3, 4])
 
 
 def test_2x2_matrices():
